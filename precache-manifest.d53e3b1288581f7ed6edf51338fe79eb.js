3ccec64a47c4c2849d6bffec4122a990a2bc17a3
lily <lily.chan@veden.dental>
2020-09-14 15:17:13 +0800

    Updates

---
+++ precache-manifest.d53e3b1288581f7ed6edf51338fe79eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7bc29b46414cd6edaa0d88fb4ea1f0",
+    "url": "/LilyChan/react-tutorial/index.html"
+  },
+  {
+    "revision": "cb32794dec7bc3377b66",
+    "url": "/LilyChan/react-tutorial/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/LilyChan/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LilyChan/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb32794dec7bc3377b66",
+    "url": "/LilyChan/react-tutorial/static/js/main.f6a60aa9.chunk.js"
+  },
+  {
+    "revision": "837d26e8e9ec2117ced7",
+    "url": "/LilyChan/react-tutorial/static/js/runtime-main.d81fba55.js"
+  }
+]);
